039ff60833da243a73dc91b741b145a73177b23a
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-07-14 21:28:03 +0200

    implement backend part

--- integrate.rb
+++ integrate.rb
@@ -3,27 +3,52 @@
 
 # make sure we have the permalink in our topic
 module PermalinkTopicViewExtender
+
     def self.included(klass)
         klass.attributes :permalink
     end
+
     def permalink
         object.topic.custom_fields["permalink"] if object.topic.custom_fields && object.topic.custom_fields.has_key?("permalink")
     end
+
 end
 
 module PermalinkExtender
+
     def self.included(klass)
         klass.attributes :permalink
     end
+
     def permalink
         object.custom_fields["permalink"] if object.custom_fields && object.custom_fields.has_key?("permalink")
     end
+
 end
 
 TopicViewSerializer.send(:include, PermalinkTopicViewExtender)
 BasicTopicSerializer.send(:include, PermalinkExtender)
 
-# And mount the engine
-# Discourse::Application.routes.append do
-#     mount Tagger::Engine, at: ':permalink'
-# end
+module TopicPermalinkExtender
+
+    def self.included(klass)
+        klass.skip_before_filter :check_xhr, only: [:permalink]
+    end
+
+    def permalink
+        field = TopicCustomField.where(:name=> 'permalink', :value=> "/" + params[:permalink]).first
+        raise Discourse::NotFound unless field
+        if SiteSetting.parmalink_redirect
+            redirect_to field.topic.url
+        else
+            params[:id] = field.topic_id
+            show
+        end
+    end
+end
+
+TopicsController.send(:include, TopicPermalinkExtender)
+
+Discourse::Application.routes.append do
+    get "*permalink" => "topics#permalink"
+end
